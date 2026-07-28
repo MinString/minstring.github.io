643aa0d084f1226e481ff5b12e35cc0020741693
MinString <x1960338651@hotmail.com>
2026-07-28 19:57:56 +0800

    添加了过去的文章，修改了部分目录

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -6,15 +6,15 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true, // Display the banner image on the homepage
+		src: "assets/images/111024784_p0.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
+			text: "Welcome!", // Credit text to be displayed
 			url: "", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
@@ -23,11 +23,10 @@ export const siteConfig: SiteConfig = {
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
 	favicon: [
-		// Leave this array empty to use the default favicon
 		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
+		// 	src: "/favicon/icon.png", // Path of the favicon, relative to the /public directory
+		// 	theme: "light", // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+		// 	sizes: "32x32", // (Optional) Size of the favicon, set only if you have favicons of different sizes
+		// },
 	],
 };
