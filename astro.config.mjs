ce7fe3ae48fd82ff3c072e021a69fae7cba3ff13
MinString <x1960338651@hotmail.com>
2026-07-28 20:11:16 +0800

    添加了Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,10 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://myblog.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
+	output: "static",
 
 	integrations: [
 		tailwind({
